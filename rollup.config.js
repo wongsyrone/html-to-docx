79ae1cd9334125ab2f7a122e5a7a87d52ab3f04c
Syrone Wong <wong.syrone@gmail.com>
2022-11-26 19:09:58 +0800

    chore: es5 iife
    iife

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,35 @@
 import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import cleaner from 'rollup-plugin-cleaner';
 import builtins from 'rollup-plugin-node-builtins';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 import * as meta from './package.json';
 
 export default {
   input: 'index.js',
-  external: ['color-name', 'escape-html', 'html-to-vdom', 'jszip', 'virtual-dom', 'xmlbuilder2'],
+  //external: ['color-name', 'escape-html', 'html-to-vdom', 'jszip', 'virtual-dom', 'xmlbuilder2'],
   plugins: [
-    resolve(),
+    builtins(),
+    
+    resolve({
+			preferBuiltins: true,
+			browser: true,
+		}),
     json(),
     commonjs(),
-    builtins(),
-    terser({
-      mangle: false,
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env'],
     }),
+    nodePolyfills( /* options */ ),
+    // terser({
+    //   mangle: false,
+    // }),
+    
     cleaner({
       targets: ['./dist/'],
     }),
@@ -27,7 +39,7 @@ export default {
       file: 'dist/html-to-docx.esm.js',
       format: 'es',
       sourcemap: true,
-      banner: `// ${meta.homepage} v${meta.version} Copyright ${new Date().getFullYear()} ${
+      banner: `// ${meta.homepage} v${meta.version} ESM Copyright ${new Date().getFullYear()} ${
         meta.author
       }`,
     },
@@ -36,9 +48,19 @@ export default {
       format: 'umd',
       name: 'HTMLToDOCX',
       sourcemap: true,
-      banner: `// ${meta.homepage} v${meta.version} Copyright ${new Date().getFullYear()} ${
+      banner: `// ${meta.homepage} v${meta.version} UMD Copyright ${new Date().getFullYear()} ${
         meta.author
       }`,
+    },
+    {
+      file: 'dist/html-to-docx.iife.es5.js',
+      format: 'iife',
+      name: 'HTMLToDOCX',
+      sourcemap: true,
+      banner: `// ${meta.homepage} v${meta.version} IIFE Copyright ${new Date().getFullYear()} ${
+        meta.author
+      }`,
+
     },
   ],
 };
